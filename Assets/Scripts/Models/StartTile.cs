1c83aa10c42eef474eba099824c78779c25f864b
Anthony Timmers <anthonytimmers@hotmail.com>
2017-07-04 18:07:44 +0200

    Add TileManager

---
+++ Assets/Scripts/Models/StartTile.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Enumerations;
+
+namespace Assets.Scripts.Models
+{
+	public class StartTile
+		: Tile
+	{
+		public Tile Construct()
+		{
+			Type = TileType.Regular;
+			Orientation = Orientation.North;
+
+			return this;
+		}
+	}
+}
